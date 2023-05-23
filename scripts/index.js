f00e62fc9f8decbdf089e975052a825ff9b694e1
Dara Oladosu <daradosu@gmail.com>
2023-05-23 17:24:55 +0100

    make sure that status url is correct for tweets containing a photo

--- scripts/index.js
+++ scripts/index.js
@@ -59,8 +59,7 @@ let addQuotedRepliesButtonHoverTitle = (quotedRepliesButton) => {
 let getStatusId = (article) => {
   let username = article.querySelector('[data-testid="User-Name"]').querySelector('a').href.split('/').pop();
   let hrefs = Array.from(article.querySelectorAll('a[href*=status]')).map((el) => el.href);
-  let hrefsForUser = hrefs.filter((href) => href.includes(username));
-  let statusHref = hrefsForUser.find((href) => href.includes('/status/'));
+  let statusHref = hrefs.filter((href) => href.match(`https:\/\/twitter.com\/${username}\/status\/[0-9]+$`)).pop();
   let statusId = statusHref.split('/').pop();
 
   return statusId;
